9c742d142bd24a7a0e8e1fdee4be4db55db1c6eb
AKvin <kevin.alvespinto@gmail.com>
2020-12-11 15:18:28 +0100

    end

--- main.py
+++ main.py
@@ -9,11 +9,31 @@ v1 = models.Vehicle('Fiat', 'red')
 v2 = models.Vehicle('Mercedes', 'black')
 h1 = models.Human("Foe", 18)
 g1 = models.Garage(12)
+m1 = models.Moto('Ducati', 'red')
+h2 = models.Human("John", 45)
+c1 = models.Car('Volvo', 'vert')
+h3 = models.Human("John", 70)
+h4 = models.Human("Doe", 30)
+h5 = models.Human("Marie", 10)
+h6 = models.Human("Curie", 52)
 
 
-def print_vehicle():
-    # Use a breakpoint in the code line below to debug your script.
-    print(v1)
+def wtprint(type, wtp):  # wtp = what to print
+    if type == "vehicle":
+        print('# Print Vehicle #')
+        print(wtp)
+    elif type == "human":
+        print('# Print Human #')
+        print(wtp)
+    elif type == "moto":
+        print('# Print Moto #')
+        print(wtp)
+    elif type == "car":
+        print('# Print Car #')
+        print(wtp)
+    elif type == "garage":
+        print('# Print Garage #')
+        print(wtp)
 
 
 def start_engine():
@@ -29,11 +49,6 @@ def stop_engine():
 def roll():
     print('# Roll #')
     v1.roll(5)
-
-
-def print_human():
-    print('# Print Human #')
-    print(h1)
 
 
 def add_vehicle_to_human(vehicle):
@@ -81,30 +96,96 @@ def set_vehicle_garage():
     g1.__setitem__(0, v2)
 
 
+def attach_moto(condition):
+    print('# Attach moto #')
+    m1.set_attach(condition)
+
+
+def goat():
+    print('# Goat moto #')
+    m1.goat(5)
+
+
+def start_moto():
+    print('# Start moto engine #')
+    m1.start_engine()
+
+
+def add_passenger(type, passenger):
+    if type == "moto":
+        print('# Add passenger to moto #')
+        m1.add_passenger(passenger)
+    elif type == "car":
+        print('# Add passenger to car #')
+        c1.add_passenger(passenger)
+
+
+def remove_passenger(type, passenger):
+    if type == "moto":
+        print('# Remove passenger to moto #')
+        m1.remove_passenger(passenger)
+    elif type == "car":
+        print('# Remove passenger to car #')
+
+
+def add_meter_credit(car, credit):
+    print('# Add meter credit to car #')
+    car.add_meter_credit(credit)
+
+
+def use_meter_credit(car, credit):
+    print('# Use credit meter car #')
+    car.use_meter_credit(credit)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_vehicle()
+    wtprint("vehicle", v1)
     start_engine()
     roll()
     stop_engine()
-    print_human()
+    wtprint("human", h1)
     human_start_car()
     human_stop_car()
     add_vehicle_to_human(v1)
-    print_human()
+    wtprint("human", h1)
     human_start_car()
     human_stop_car()
-    print_garage()
+    wtprint("garage", g1)
     human_own_garage()
-    print_human()
-    print_garage()
+    wtprint("human", h1)
+    wtprint("garage", g1)
     add_vehicle_garage()
     deposit_garage()
-    print_garage()
+    wtprint("garage", g1)
     get_vehicle_garage()
     set_vehicle_garage()
     get_vehicle_garage()
-    print_garage()
+    wtprint("garage", g1)
+    wtprint("moto", m1)
+    attach_moto(True)
+    start_moto()
+    wtprint("moto", m1)
+    goat()
+    attach_moto(False)
+    goat()
+    add_passenger("moto", h1)
+    start_moto()
+    goat()
+    add_passenger("moto", h2)
+    add_passenger("moto", h3)
+    wtprint("moto", m1)
+    wtprint("car", c1)
+    add_passenger("car", h1)
+    add_passenger("car", h2)
+    add_passenger("car", h3)
+    add_passenger("car", h4)
+    add_passenger("car", h5)
+    add_passenger("car", h6)
+    add_meter_credit(c1, 50)
+    use_meter_credit(c1, 25)
+    use_meter_credit(c1, 25)
+    use_meter_credit(c1, 50)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+import random
 
 # Vehicle class ##########
 
 
 class Vehicle:
-    """Classe définissant une voiture caractérisée par :
+    """Classe définissant un vehicle caractérisée par :
     - brand : marque du véhicule
     - color : couleur du véhicule
     - year : année de la date réel de la création du véhicule
@@ -73,14 +74,238 @@ class Vehicle:
     def __repr__(self):
         return '{marque : ' + self.get_brand() + ', couleur : ' + self.get_color() \
                + ', année : ' + str(self.get_year()) \
-               + ', nb km : ' + str(self.get_km()) + ', état : ' + str(self.get_condition()) + '}'
+               + ', nb km : ' + str(self.get_km()) + ', démarré : ' + str(self.get_condition()) + '}'
 
     def __str__(self):
         return 'Vehicle {\n\tmarque : ' + self.get_brand() \
                + '\n\tcouleur : ' + self.get_color() \
                + '\n\tannée : ' + str(self.get_year()) \
                + '\n\tnb km : ' + str(self.get_km()) \
-               + '\n\tétat : ' + str(self.get_condition()) + '\n\t}'
+               + '\n\tdémarré : ' + str(self.get_condition()) + '\n\t}'
+
+# Moto class ##########
+
+
+class Moto(Vehicle):
+    """Classe définissant une Moto héritant de la class Vehicle caractérisée par :
+        - type : type du véhicule
+        - nb_wheel : nombre de roue du véhicule
+        - nb_passenger : nombre de passagers disponible du véhicule
+        - passengers : les passagers du véhicule
+        - attach : condition du véhicule True = attaché False = détaché
+        """
+
+    def __init__(self, brand, color):
+        Vehicle.__init__(self, brand, color)
+        self._type = "Moto"
+        self._nb_wheel = 2
+        self._nb_passenger = 2
+        self.passengers = []
+        self._attach = False
+
+    def get_type(self):
+        return self._type
+
+    def get_nb_wheel(self):
+        return self._nb_wheel
+
+    def get_nb_passenger(self):
+        return self._nb_passenger
+
+    def get_passengers(self):
+        return self.passengers
+
+    def available_space(self):
+        if self.get_nb_passenger() > len(self.get_passengers()):
+            return True
+        else:
+            return False
+
+    def add_passenger(self, passenger):
+        if self.available_space():
+            self.passengers.append(passenger)
+            print("=> Passenger added to moto vehicle successfully !")
+        else:
+            print("=> Couldn't add passenger to moto vehicle, No more available space !")
+
+    def remove_passenger(self, passenger):
+        if not self.available_space():
+            self.passengers.remove(passenger)
+            print("=> Remove passenger from moto vehicle successfully !")
+        else:
+            print("=> Couldn't remove passenger from moto vehicle, Available space empty !")
+
+    def get_attach(self):
+        return self._attach
+
+    def set_attach(self, condition):
+        if condition:
+            print("=> Moto attached !")
+        else:
+            print("=> Moto detached !")
+        self._attach = condition
+
+    def goat(self, km):
+        if self.get_passengers():
+            if not self.get_attach():
+                if random.randint(0, 100) <= 10:
+                    print("=> Your have crashed dummy !")
+                else:
+                    self.roll(km)
+            else:
+                print("=> You can't goat, moto is attached !")
+        else:
+            print("=> You can't goat, moto have no passenger !")
+
+    def start_engine(self):
+        if not self.get_attach():
+            if not self.get_condition():
+                self.set_condition(True)
+                print("=> Engine started !")
+            else:
+                print("=> Engine already started !")
+        else:
+            print("=> Detach moto before starting engine !")
+
+    def __repr__(self):
+        return '{type : ' + self.get_type() \
+               + ', nb roues : ' + str(self.get_nb_wheel())\
+               + ', nb passagers : ' + str(self.get_nb_passenger()) \
+               + ', marque : ' + self.get_brand() \
+               + ', couleur : ' + self.get_color() \
+               + ', année : ' + str(self.get_year()) \
+               + ', nb km : ' + str(self.get_km()) \
+               + ', démarré : ' + str(self.get_condition()) \
+               + ', attaché : ' + str(self.get_attach()) + '}'
+
+    def __str__(self):
+        return 'Moto {\n\ttype : ' + self.get_type() \
+               + '\n\tnb roues : ' + str(self.get_nb_wheel()) \
+               + '\n\tnb passagers : ' + str(self.get_nb_passenger()) \
+               + '\n\tpassagers : ' + str(self.get_passengers()) \
+               + '\n\tmarque : ' + self.get_brand() \
+               + '\n\tcouleur : ' + self.get_color() \
+               + '\n\tannée : ' + str(self.get_year()) \
+               + '\n\tnb km : ' + str(self.get_km()) \
+               + '\n\tdémarré : ' + str(self.get_condition()) \
+               + '\n\tattaché : ' + str(self.get_attach()) + '\n\t}'
+
+    def __len__(self):
+        # return len(self.vehicules)
+        return self.get_passengers().__len__()
+
+# Car class ##########
+
+
+class Car(Vehicle):
+    """Classe définissant une voiture héritant de la class Vehicle caractérisée par :
+        - type : type du véhicule
+        - nb_wheel : nombre de roue du véhicule
+        - nb_passenger : nombre de passagers du véhicule
+        - passengers : les passagers du véhicule
+        - meter_credit : le credit dans le parcmètre
+        - use_credit : le credit utilisé
+        - counter : compte le nombre d'instance
+        """
+    counter = 0
+
+    def __init__(self, brand, color):
+        Vehicle.__init__(self, brand, color)
+        self._type = "Voiture"
+        self._nb_wheel = 4
+        self._nb_passenger = 5
+        self.passengers = []
+        self._meter_credit = 0
+        self._use_credit = 0
+        type(self).counter += 1
+
+    def get_type(self):
+        return self._type
+
+    def get_nb_wheel(self):
+        return self._nb_wheel
+
+    def get_nb_passenger(self):
+        return self._nb_passenger
+
+    def get_passengers(self):
+        return self.passengers
+
+    def available_space(self):
+        if self.get_nb_passenger() > len(self.get_passengers()):
+            return True
+        else:
+            return False
+
+    def add_passenger(self, passenger):
+        if self.available_space():
+            self.passengers.append(passenger)
+            print("=> Passenger added to car vehicle successfully !")
+        else:
+            print("=> Couldn't add passenger to car vehicle, No more available space !")
+
+    def remove_passenger(self, passenger):
+        if not self.available_space():
+            self.passengers.remove(passenger)
+            print("=> Remove passenger from car vehicle successfully !")
+        else:
+            print("=> Couldn't remove passenger from car vehicle, Available space empty !")
+
+    def get_meter_credit(self):
+        return self._meter_credit
+
+    def set_meter_credit(self, credit):
+        self._meter_credit = credit
+
+    def add_meter_credit(self, credit):
+        print("=> you add " + str(credit) + " credit successfully !")
+        self._meter_credit += credit
+
+    def get_use_credit(self):
+        return self._use_credit
+
+    def use_meter_credit(self, credit):
+        if self.get_meter_credit() >= credit:
+            if not (self.get_meter_credit()) - credit <= 0:
+                print("=> You have enought credit !")
+                self._meter_credit -= credit
+                print("=> Meter credit decremente by " + str(credit) + " credit !")
+                self._use_credit += credit
+                print("=> Used credit increment by " + str(credit) + " credit !")
+            else:
+                print("=> Can't reach 0, add credit ! ")
+        else:
+            print("=> You haven't enought credit !")
+
+    def __repr__(self):
+        return '{type : ' + self.get_type() \
+               + ', nb roues : ' + str(self.get_nb_wheel())\
+               + ', nb passagers : ' + str(self.get_nb_passenger()) \
+               + ', marque : ' + self.get_brand() \
+               + ', couleur : ' + self.get_color() \
+               + ', année : ' + str(self.get_year()) \
+               + ', nb km : ' + str(self.get_km()) \
+               + ', démarré : ' + str(self.get_condition()) \
+               + ', compteur d''instance : ' + str(type(self).counter) + '}'
+
+    def __str__(self):
+        return 'Car {\n\ttype : ' + self.get_type() \
+               + '\n\tnb roues : ' + str(self.get_nb_wheel()) \
+               + '\n\tnb passagers : ' + str(self.get_nb_passenger()) \
+               + '\n\tpassagers : ' + str(self.get_passengers()) \
+               + '\n\tmarque : ' + self.get_brand() \
+               + '\n\tcouleur : ' + self.get_color() \
+               + '\n\tannée : ' + str(self.get_year()) \
+               + '\n\tnb km : ' + str(self.get_km()) \
+               + '\n\tdémarré : ' + str(self.get_condition()) \
+               + '\n\tcompteur d''instance : ' + str(type(self).counter) + '\n\t}'
+
+    def __len__(self):
+        # return len(self.vehicules)
+        return self.get_passengers().__len__()
+
+    def __del__(self):
+        type(self).counter -= 1
 
 
 # Human class ##########
